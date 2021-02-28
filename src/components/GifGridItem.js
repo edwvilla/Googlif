aa70ddd1ee654cf0b17152408648bfe69489eeff
Edwin Villa <villaceniceros@gmail.com>
2021-02-27 23:18:35 -0700

    changes

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "animate.css";
 
 export const GifGridItem = ({ title, url }) => {
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  }, [copied]);
+
   return (
-    <div className="card animate__animated animate__bounceIn">
+    <div
+      className="card animate__animated animate__bounceIn"
+      onClick={() => {
+        navigator.clipboard.writeText(url);
+        setCopied(true);
+      }}
+    >
       <img src={url} alt={title} />
-      <p>{title}</p>
+      {copied ? <p className="green">Copied to clipboard!</p> : <p>{title}</p>}
     </div>
   );
 };
